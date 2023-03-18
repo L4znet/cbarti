ec951c4db9a1a4a85ac2553cb5b514193a74fe41
Charly Escalona <charly.escalona1@gmail.com>
2023-03-18 13:47:04 +0100

    add somes links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,14 @@
 import Profil from "./Profile";
 import Profile from "./Profile";
+import {Link} from "react-router-dom";
 
 export default function Navbar(){
     return (
         <>
             <nav>
                 <ul>
-                    <li><a href="">Un bar random</a></li>
-                    <li><a href="">Ajouter un lieu</a></li>
+                    <li><Link to={"/random"}>Un bar random</Link></li>
+                    <li><Link to={"/new"}>Ajouter un lieu</Link></li>
                 </ul>
                 <Profile id="1" username="Laznet"/>
             </nav>
